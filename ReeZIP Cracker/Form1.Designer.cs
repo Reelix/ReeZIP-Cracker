f5f32034cbdfeb952770ab24c225d4f660ab6e3f
Reelix <reelix@gmail.com>
2017-12-28 08:59:42 +0200

    Initial commit

---
+++ ReeZIP Cracker/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ReeZIP_Cracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenZIP = new System.Windows.Forms.Button();
+            this.txtZIP = new System.Windows.Forms.TextBox();
+            this.lbResults = new System.Windows.Forms.ListBox();
+            this.ofdZIP = new System.Windows.Forms.OpenFileDialog();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnOpenDictionary = new System.Windows.Forms.Button();
+            this.txtDictionary = new System.Windows.Forms.TextBox();
+            this.ofdDictionary = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnOpenZIP
+            // 
+            this.btnOpenZIP.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenZIP.Location = new System.Drawing.Point(32, 39);
+            this.btnOpenZIP.Name = "btnOpenZIP";
+            this.btnOpenZIP.Size = new System.Drawing.Size(117, 32);
+            this.btnOpenZIP.TabIndex = 0;
+            this.btnOpenZIP.Text = "Choose ZIP File";
+            this.btnOpenZIP.UseVisualStyleBackColor = true;
+            this.btnOpenZIP.Click += new System.EventHandler(this.btnOpenZIP_Click);
+            // 
+            // txtZIP
+            // 
+            this.txtZIP.BackColor = System.Drawing.Color.Black;
+            this.txtZIP.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtZIP.ForeColor = System.Drawing.Color.Yellow;
+            this.txtZIP.Location = new System.Drawing.Point(32, 81);
+            this.txtZIP.Name = "txtZIP";
+            this.txtZIP.Size = new System.Drawing.Size(471, 27);
+            this.txtZIP.TabIndex = 1;
+            // 
+            // lbResults
+            // 
+            this.lbResults.BackColor = System.Drawing.Color.Black;
+            this.lbResults.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResults.ForeColor = System.Drawing.Color.Yellow;
+            this.lbResults.FormattingEnabled = true;
+            this.lbResults.ItemHeight = 16;
+            this.lbResults.Location = new System.Drawing.Point(32, 233);
+            this.lbResults.Name = "lbResults";
+            this.lbResults.Size = new System.Drawing.Size(471, 116);
+            this.lbResults.TabIndex = 2;
+            // 
+            // ofdZIP
+            // 
+            this.ofdZIP.Filter = "ZIP Files|*.zip";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(32, 204);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.Black;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.Yellow;
+            this.txtPassword.Location = new System.Drawing.Point(133, 378);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(370, 29);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.Text = "Click \"Go\" above";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.Yellow;
+            this.lblPassword.Location = new System.Drawing.Point(29, 385);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(98, 18);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password: ";
+            // 
+            // btnOpenDictionary
+            // 
+            this.btnOpenDictionary.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenDictionary.Location = new System.Drawing.Point(32, 114);
+            this.btnOpenDictionary.Name = "btnOpenDictionary";
+            this.btnOpenDictionary.Size = new System.Drawing.Size(227, 32);
+            this.btnOpenDictionary.TabIndex = 6;
+            this.btnOpenDictionary.Text = "Choose Dictionary File";
+            this.btnOpenDictionary.UseVisualStyleBackColor = true;
+            this.btnOpenDictionary.Click += new System.EventHandler(this.btnOpenDictionary_Click);
+            // 
+            // txtDictionary
+            // 
+            this.txtDictionary.BackColor = System.Drawing.Color.Black;
+            this.txtDictionary.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDictionary.ForeColor = System.Drawing.Color.Yellow;
+            this.txtDictionary.Location = new System.Drawing.Point(32, 152);
+            this.txtDictionary.Name = "txtDictionary";
+            this.txtDictionary.Size = new System.Drawing.Size(471, 27);
+            this.txtDictionary.TabIndex = 7;
+            // 
+            // ofdDictionary
+            // 
+            this.ofdDictionary.Filter = "Dictionary List|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(530, 440);
+            this.Controls.Add(this.txtDictionary);
+            this.Controls.Add(this.btnOpenDictionary);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lbResults);
+            this.Controls.Add(this.txtZIP);
+            this.Controls.Add(this.btnOpenZIP);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Reelix\'s ZIP Cracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenZIP;
+        private System.Windows.Forms.TextBox txtZIP;
+        private System.Windows.Forms.ListBox lbResults;
+        private System.Windows.Forms.OpenFileDialog ofdZIP;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnOpenDictionary;
+        private System.Windows.Forms.TextBox txtDictionary;
+        private System.Windows.Forms.OpenFileDialog ofdDictionary;
+    }
+}
+
---
+++ ReeZIP Cracker/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.IO;
+using System.Threading;
+
+namespace ReeZIP_Cracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenZIP_Click(object sender, EventArgs e)
+        {
+            ofdZIP.InitialDirectory = Application.StartupPath;
+            DialogResult theResult = ofdZIP.ShowDialog();
+            if (theResult == DialogResult.OK)
+            {
+                string thePath = ofdZIP.FileName;
+                txtZIP.Text = thePath;
+            }
+        }
+
+        private void btnOpenDictionary_Click(object sender, EventArgs e)
+        {
+            ofdDictionary.InitialDirectory = Application.StartupPath;
+            DialogResult theResult = ofdDictionary.ShowDialog();
+            if (theResult == DialogResult.OK)
+            {
+                string thePath = ofdDictionary.FileName;
+                txtDictionary.Text = thePath;
+            }
+        }
+
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            // Make sure the files exist (They can be entered manually)
+            if (!File.Exists(txtZIP.Text))
+            {
+                MessageBox.Show("ZIP file does not exist!");
+                return;
+            }
+
+            if (!File.Exists(txtDictionary.Text))
+            {
+                MessageBox.Show("Dictionary file does not exist!");
+                return;
+            }
+
+            // Disable the buttons
+            btnOpenZIP.Enabled = false;
+            btnOpenDictionary.Enabled = false;
+            btnGo.Enabled = false;
+
+            // Initialise UI
+            txtPassword.Text = "Cracking...";
+            txtPassword.Refresh();
+
+            // And go!
+            Thread crackingThread = new Thread(doCracking);
+            crackingThread.IsBackground = true; // So it closes when the app does
+            crackingThread.Start();
+        }
+
+        void doCracking()
+        {
+            DateTime startTime = DateTime.Now;
+            string zipPath = txtZIP.Text;
+            string dictionaryPath = txtDictionary.Text;
+            addListboxItem("Loading Dictionary...");
+            List<string> passList = File.ReadLines(dictionaryPath).ToList();
+            addListboxItem("Starting cracking...");
+            bool finished = false;
+            using (ZipFile archive = ZipFile.Read(zipPath))
+            {
+                int tryPass = 0;
+
+                foreach (ZipEntry file in archive)
+                {
+                    while (!finished)
+                    {
+                        string password = passList[tryPass];
+                        tryPass++;
+                        try
+                        {
+                            file.ExtractWithPassword(Application.StartupPath, password);
+                            // If it passes this - It's cracked :D
+                            DateTime endTime = DateTime.Now;
+                            TimeSpan timeDiff = endTime - startTime;
+                            addListboxItem("Success With: " + password + " (" + tryPass + " attempts after " + Math.Truncate(timeDiff.TotalSeconds) + " seconds)");
+                            txtPassword.Text = password;
+                            MessageBox.Show("Password: " + password, "Password Found!");
+                            // Clean up
+                            File.Delete(Application.StartupPath + file.FileName);
+                            finished = true;
+                        }
+                        catch (Ionic.Zlib.ZlibException)
+                        {
+                            // We don't care about these
+                        }
+                        catch (Ionic.Zip.BadReadException)
+                        {
+                            // Or these
+                        }
+                        catch (Ionic.Zip.BadCrcException)
+                        {
+                            // Or these....
+                        }
+                        catch (Ionic.Zip.BadPasswordException)
+                        {
+                            // We do care about this one though!
+                            if (tryPass % 1000 == 0)
+                            {
+                                addListboxItem("Failed With: " + password + " (" + tryPass + " attempts...)");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            // A variation of the BadPasswordException - Weird....
+                            if (ex.Message == "The password did not match.")
+                            {
+                                if (tryPass % 1000 == 0)
+                                {
+                                    addListboxItem("Failed With: " + password + " (" + tryPass + " attempts...)");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error: " + ex.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void addListboxItem(string item)
+        {
+            // Threading issues with the UI
+            lbResults.Invoke(new Action(() => lbResults.Items.Insert(0, item)));
+            lbResults.Invoke(new Action(() => lbResults.Refresh()));
+        }
+    }
+}
